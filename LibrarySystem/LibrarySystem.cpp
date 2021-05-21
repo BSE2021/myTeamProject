e5f06769377cf208456840b0058bd5df74116430
beulathamban <beulathamban@gmail.com>
2021-05-21 16:21:57 +1200

    initial commit

---
+++ LibrarySystem/LibrarySystem.cpp
@@ -0,0 +1,25 @@
+// LibrarySystem.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+//Jakob's code
+//function//===================
+
+
+//==========================
+//Jacky's code
+int main()
+{
+	//Jakob
+	//============
+   
+
+
+
+	//===========
+}
+
